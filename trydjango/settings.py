9c5c26a71a97758b8626eccac3a8c10229fd9e83
AryanVerma <aryan2003.2011@gmail.com>
2020-10-23 23:29:48 +0530

    did heroki

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -53,6 +53,7 @@ CRISPY_TEMPLATES_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku,settings(locals())
+
 MEDIA_ROOT = os.path.join(BASE_DIR , "media")
 
 MEDIA_URL = '/media/'
